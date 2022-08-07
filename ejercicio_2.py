0b1054129962eeb100d5990a0cb02629eca85ef2
CharlieV22 <109441661+CharlieV22@users.noreply.github.com>
2022-08-07 11:42:44 -0300

    Add files via upload

---
+++ ejercicio_2.py
@@ -0,0 +1,32 @@
+# Bucles [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejemplos con bucles "for"
+
+# Dado la siguiente lista de colores, utilizar "for"
+# para imprimir en pantalla todos los colores
+colores = ['rojo', 'naranja', 'verde', 'azul']
+
+
+# Itere el "for" utilizando la lista como parámero
+# y utilizar como elemento del "for" cada color
+# for color ...
+for color in colores:
+    print("el color es", color)
+# Itere el "for" utilizando el tamaño de la lista
+# como parámetro y utilizar el índice para acceder a
+# los elementos de la lista
+# for i ...
+colores_len = len(colores)
+for i in range(colores_len):
+    print("El color es", colores [i])
+
+
+
+print("terminamos!")
